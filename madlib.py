a203fb5e1f389f8891b9e55785c697c9c0555d6e
cekaralar <113308416+cekaralar@users.noreply.github.com>
2022-09-11 23:16:04 +0300

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -1,4 +1,5 @@
 adj = input("Adjective :")
 verb1 = input("Verb :")
-verb2 = ("Verb :")
+verb2 = input("Verb :")
 madlib = f"computer programming is so {adj} and I like doing {verb1} with {verb2}"
+print(madlib)
